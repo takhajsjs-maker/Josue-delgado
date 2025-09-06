3c3f3dcc3f1bfc1002c890b56a1abf8d1f1b0c85
takhajsjs-maker <takhajsjs@gmail.com>
2025-09-06 16:30:47 -0400

    Add files via upload

--- tarea.py
+++ tarea.py
@@ -1,14 +1,6 @@
-# de donde son, que edad tienen, y cual es tu nombre, y si tienes mas de 18 o menos.
-
-lugar = input("¿De dónde eres? ") # where do they live
-edad = int(input("¿Cuántos años tienes? ")) # how old are they   
-nombre = input("¿Cuál es tu nombre? ") # what is your name
-if  edad < 18:
-    print("eres menor de edad")
-elif edad == 18:
-    print("tienes 18 ")
+edad = int(input("ingrese su edad "))
+inscrito = input("¿estas inscrito en el CNE? (si/no):  ").lower()
+if edad >= 18 and inscrito == "si":
+    print(" usted tiene la edad para votar y esta en inscrito en el CNE.")
 else:
-    print("eres mayor de edad")
-
-print(f"holaa {nombre}, un gusto, que edad tienes, {edad}, y donde vives, {lugar}") 
-      
+    print("usted no cumple con los requisitos para.")    
