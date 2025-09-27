948e79f3f3461d752b89d293e96ee1d493eae8dd
takhajsjs-maker <takhajsjs@gmail.com>
2025-09-27 19:34:31 -0400

    Add files via upload

---
+++ tarea.js
@@ -0,0 +1,86 @@
+/* ejercicio 1: declaracion de variables
+-*Enunciado:* crea un script que declare tu nombre, año de nacimiento y año actual.
+calcula tu edad y muestrala en la consola!*/
+
+/*const nombre = "josue delgado";
+const año_de_nacimiento = 2006;
+const año_actual = 2024;
+
+const edad = año_actual - año_de_nacimiento;
+console.log("Mi nombre es " + nombre + " y tengo " + edad + " años.");*/
+
+
+
+
+/*Ejercicio 2: Operadores y Condicionales 🗳
+- Enunciado: Pide al usuario su edad y dile si es mayor de edad o no.
+- Requerimientos: Usa prompt(), convierte la entrada a number, y utiliza una estructura if-else.*/
+
+/*const edad = Number;
+const number = prompt("¿Cual es tu edad?");
+
+if (number >= 18) {
+    console.log("eres mayor de edad");
+} else {
+    console.log("eres menor de edad");
+}*/
+
+
+
+/*Ejercicio 3: Manipulación de Strings ✨
+- Enunciado: Pide nombre y apellido por separado, únelos en una variable de nombre completo y muéstralo en MAYÚSCULAS.
+- Requerimientos: Dos prompt(), concatenación (con + o `` ${} ``) y el método .toUpperCase().*/
+
+/*const nombre = prompt("¿Cual es tu nombre?");
+const apellido = prompt("Cual es tu apellido?");
+conts nombre_completo = nombre + " " + apellido;
+
+console.log|(nombre_completo.toUpperCase());*/
+
+
+
+
+
+
+
+/*Ejercicio 5: Bucle for Básico* 🔟
+- Enunciado: ¡Simple! Usa un bucle for para imprimir los números del 1 al 10 en la consola.
+- Requerimientos: Estructura for clásica con inicialización, condición e incremento.*/
+
+/*for (let i = 1; i <= 10; i++){
+    console.log(i);
+}*/
+
+
+
+
+
+
+/*Ejercicio 6: Condicionales Anidados 🎓
+- Enunciado: Pide una nota del 0 al 10 y conviértela a calificación: Suspenso, Aprobado, Notable o Sobresaliente.
+- Requerimientos: Usa if-elif-else, operadores lógicos && y maneja entradas no válidas.*/
+
+/*const nota = Number
+const number = prompt("ingresa una nota del 0 al 10");
+
+if (number>=0 && number<4) {
+    console.log("suspenso");
+}
+else if (Number>=4 77 number <7) {
+    console.log("aprobado");
+}
+else if (number >7 && number <9) {
+    console.log("notable");
+}
+else if (number > 9 && number <=10) {
+    console.log("sobresaliente");
+}
+*/
+
+
+
+
+
+
+
+
